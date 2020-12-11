1185e6db2bb3f85ef55a4bf294eda9273fd9ceae
Marcin Chamera <MarcinChamera@github.com>
2020-12-11 13:39:19 +0100

    poprawione zadanie z rownaniem kwadratowym wersja 02

--- lekcja08/zad8_1.py
+++ lekcja08/zad8_1.py
@@ -1,18 +1,22 @@
-import cmath
+import math
 
 def solve2(a, b, c):
 	"""Rozwiązywanie równania kwadratowego a * x * x + b * x + c = 0."""
 	if a == 0:
-		return -c / b
+		if b == 0:
+			return (None, None)
+		return (-c / b, None)
 	d = b ** 2 - 4 * a * c
 	if d < 0:
 		raise ValueError("ujemna delta")
 		# return (None, None)
 	if d == 0:
-		return (-b / (2 * a), None)
-	return ((-b - d) / (2 * a), (-b + d) / (2 * a))
+		return (-b / (2 * a), -b / (2 * a))
+	return ((-b - math.sqrt(d)) / (2 * a), (-b + math.sqrt(d)) / (2 * a))
 
 if __name__ == '__main__':
+	print(solve2(0, 2, 5))
+	print(solve2(0, 0, 0))
 	try:
 		print(solve2(1, 1, 1))
 	except ValueError as e:
